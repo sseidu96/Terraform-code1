2d56d684ca780e28fd498601e0ed8ff31bf5d8bd
Safiatu Seidu <sseidu96>
2026-08-08 19:49:16 -0400

    Ticket number 4

---
+++ CODE1/output.tf
@@ -0,0 +1,7 @@
+output "my-group-name" {
+  value = aws_iam_group.developers
+}
+
+output "my-username" {
+  value = aws_iam_username.Admin
+}
--- CODE12/output.tf
+++ CODE12/output.tf
@@ -5,3 +5,15 @@ output "public_ip" {
 output "public_dns" {
   value = aws_instance.httpd_server.public_dns
 }
+
+output "availability_zone" {
+  value = aws_instance.httpd_server.availability_zone
+}
+
+output "private_ip" {
+  value = aws_instance.httpd_server.private_ip
+}
+
+output "security_group" {
+  value = aws_instance.httpd_server.security_groups
+}
